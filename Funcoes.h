ea62745250467267e6b1421f4748994a180a24cd
Brenddon Andrade <brenddonandrade@gmail.com>
2022-11-23 17:25:02 -0300

    :up: tudo funcionando e :package: add teste.c para testar as funcoes

--- Funcoes.h
+++ Funcoes.h
@@ -1,6 +1,12 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
 
+#include <stdio.h>
+#include <math.h>
+
+double soma(double a, double b){
+    return (a+b);
+}
 
 
 
---
+++ teste.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "./Funcoes.h"
+
+int main(){
+
+    double x=20, y=30;
+
+    printf("Soma: %.4lf\n\n", soma(x,y));
+
+    return 0;
+}
