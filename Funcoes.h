ae5bb645aa5c4983fab7ad868219f40e786e4f3b
Brenddon Andrade <brenddonandrade@gmail.com>
2022-11-30 17:46:28 -0300

    :up: adicionado o metodo de Euler Explicito

--- Funcoes.h
+++ Funcoes.h
@@ -4,6 +4,19 @@
 #include <stdio.h>
 #include <math.h>
 
+//  px e py sao os ponteiros que vao armazenar os pontos;
+void metodo_de_euler_explicito(double x1, double y1, double *px, double *py) {
+  double x2, y2;
+
+  x2 = x1 + H;
+  *px = x2;
+
+  y2 = y1 + derivada(x1, y1);
+  *py = y2;
+
+  return;
+}
+
 double bissecao(double f(), double a, double b, double *err, int *itr) {
   double m, fm, tolerance, m_a;
   int n, nmax;
